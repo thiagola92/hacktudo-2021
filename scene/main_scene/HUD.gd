6c2144fca986d4892615b82464651581d264f04a
Stephanie Fay <stephaniemfay@gmail.com>
2021-10-16 18:43:05 -0300

    passTime and passDay on Global

--- scene/main_scene/HUD.gd
+++ scene/main_scene/HUD.gd
@@ -15,65 +15,6 @@ func _process(delta):
 	$mentalHealth_fill.value = Global.humor
 	$health_fill.value = Global.health	
 
-func processHealth (newHealth):
-	$health_fill.value = newHealth
-	Global.health = newHealth
-	
-	if ($health_fill.value == 0): #game over?
-		pass
-
-func processMentalHealth (newHealth):
-	$mentalHealth_fill.value = newHealth
-	Global.humor = newHealth
-	
-	if ($mentalHealth_fill.value == 0): #game over?
-		pass
-	
-
-func addOrRemoveMoney (money):
-	var currentMoney = int($moneyLabel.text) + money
-	if (currentMoney <= 0): # game over
-		pass
-	
-	$moneyLabel.text = String(currentMoney)
-	Global.money = currentMoney
-	
-func verifyHour (currentTime, hourSpent):
-	var currentHour = currentTime[0]
-	if (currentHour + hourSpent > 23):
-		passDay()
-		currentHour = (currentHour + hourSpent) - 24
-	else:
-		currentHour = currentHour + hourSpent
-	
-	currentTime[0] = currentHour
-	return currentTime
-	
-func verifyMinute (currentTime, minuteSpent):
-	var currentMinute = currentTime[1]
-	if (currentMinute + minuteSpent > 60):
-		currentMinute = (currentMinute + minuteSpent) - 60
-		currentTime = verifyHour(currentTime, 1)
-	else:
-		currentMinute = currentMinute + minuteSpent
-		
-	currentTime[1] = currentMinute
-	return currentTime
-			
-func passTime (hourSpent, minuteSpent):
-	var strArray = $clockLabel.text.split(":")
-	var currentTime = [int(strArray[0]), int(strArray[1])]
-
-	currentTime = verifyHour(currentTime, hourSpent)
-	currentTime = verifyMinute(currentTime, minuteSpent)
-	
-	$clockLabel.text = String(currentTime[0]) + ":" + String(currentTime[1])
-	Global.time = String(currentTime[0]) + ":" + String(currentTime[1])
-	
-func passDay ():
-	Global.currentDay += 1
-
-
 func _on_computer_gui_input(event):
 	if Input.is_action_just_pressed("ui_left_click"):
 		$Window.popup_centered()
