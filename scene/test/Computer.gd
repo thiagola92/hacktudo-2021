9c22cabf496403b0646c48b22d1f197509107c4f
thiagola92 <thiagola92@gmail.com>
2021-10-15 23:37:05 -0300

    Add test pause & test menu

--- scene/test/Computer.gd
+++ scene/test/Computer.gd
@@ -9,3 +9,13 @@ func _on_Computer_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == BUTTON_LEFT:
 			print("hi :(")
+			get_tree().paused = true
+			$AcceptDialog.popup_centered()
+
+
+func _on_AcceptDialog_confirmed():
+	get_tree().paused = false
+
+
+func _on_AcceptDialog_popup_hide():
+	get_tree().paused = false
