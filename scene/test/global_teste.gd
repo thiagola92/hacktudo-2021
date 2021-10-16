a4f2dd46e9f34699d3c78a425412fb4373cd4aa0
thiagola92 <thiagola92@gmail.com>
2021-10-16 12:26:30 -0300

    Update global example

--- scene/test/global_teste.gd
+++ scene/test/global_teste.gd
@@ -1,7 +1,21 @@
 extends Node
 
 
-var actions = [
-	{'nome': 'asdf'},
-	{'nome': 'zxcv'}
-]
+var actions = []
+
+
+func _ready():
+	add_action(':)', '40', '50', '49', 'financeiro', '')
+	add_action(':)', '40', '50', '49', 'financeiro', '')
+	add_action(':)', '40', '50', '49', 'financeiro', '')
+
+
+func add_action(name, perc, ac, risc, ramo, action_type):
+	actions.append({
+		'nome': name,
+		'perc': perc,
+		'ac': ac,
+		'risc': risc,
+		'ramo': ramo,
+		'type': action_type
+	})
