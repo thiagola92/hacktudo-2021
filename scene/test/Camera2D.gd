e4fa7cc0388b416b34d86351e086a6bc70e4b2d8
thiagola92 <thiagola92@gmail.com>
2021-10-15 23:27:23 -0300

    Add test health

--- scene/test/Camera2D.gd
+++ scene/test/Camera2D.gd
@@ -7,3 +7,9 @@ func _ready():
 
 func _process(delta):
 	position.x += 1
+	
+	$Health.value -= 1
+	
+	if $Health.value == 0:
+		$Health.hide()
+		$Computer.hide()
---
+++ scene/test/Health.gd
@@ -0,0 +1,5 @@
+extends TextureProgress
+
+
+func _ready():
+	value = 100
