dffa8969a725dd76d2554372195c74fedfe68e78
Lucas Hardman <lucas.hardman92@gmail.com>
2021-10-16 18:31:40 -0300

    Refrigerator loading owned food

--- scene/test/RefrigeratorMenu/RefrigeratorMenu.gd
+++ scene/test/RefrigeratorMenu/RefrigeratorMenu.gd
@@ -11,8 +11,42 @@ func _ready():
 	# For test purpose only
 	if $"." in get_tree().get_root().get_children():
 		popup_centered()
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
+	add_food("res://image/game/foods/DISH2.png", "Bolo", "-1", "+5", 3.80)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func add_food(spr, name, health_effect, mood_effect, price):
+
+	var texture = TextureRect.new()
+	var button = Button.new()
+	var vbox = VBoxContainer.new()
+	vbox.size_flags_horizontal = SIZE_EXPAND_FILL
+	vbox.size_flags_vertical = SIZE_EXPAND_FILL
+	vbox.hint_tooltip = name+"\nVariação de fome: "+String(health_effect)+"\nVariação de humor: "+String(mood_effect)
+	
+	button.text = "Comer"
+	texture.texture = load(spr)
+	texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	texture.rect_min_size.x = 50
+	texture.rect_min_size.y = 50
+	
+	vbox.add_child(texture)
+	vbox.add_child(button)
+	$VBoxContainer/FoodGrid.add_child(vbox)
+	
