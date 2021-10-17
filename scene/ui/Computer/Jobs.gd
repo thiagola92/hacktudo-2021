ebf4e7a48d87bca4c12bb55637e7786e660958f2
thiagola92 <thiagola92@gmail.com>
2021-10-17 03:24:36 -0300

    Increase background

---
+++ scene/ui/Computer/Jobs.gd
@@ -0,0 +1,26 @@
+extends VBoxContainer
+
+
+var Job = preload("res://scene/ui/Computer/Job/Job.tscn")
+
+
+func _ready():
+	reload()
+
+
+func reload():
+	clear()
+	
+	add_jobs()
+
+
+func add_jobs():
+	for j in GlobalJobs.disponibleJobs:
+		var job = Job.instance()
+		job.set_job(j)
+		add_child(job)
+
+
+func clear():
+	for c in get_children():
+		remove_child(c)
